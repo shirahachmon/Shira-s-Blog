7a7e558abebd7c9809b3a2a263b041ec7a7ae968
shirahachmon <75565282+shirahachmon@users.noreply.github.com>
2022-08-23 20:37:20 +0300

    Adding dynamic search only in angular(yet with server as nodejs.)

--- src/app/notes/notes-list/notes-list.component.ts
+++ src/app/notes/notes-list/notes-list.component.ts
@@ -1,5 +1,5 @@
 import { style, transition, trigger, animate, query, stagger} from '@angular/animations';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import {faUserLock, faSearch} from '@fortawesome/free-solid-svg-icons';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
@@ -92,15 +92,105 @@ export class NotesListComponent implements OnInit {
   // cardBody: string= 'body some bodybody some bodybody some body....'
 
   notes: Note[]= new Array<Note>();
+  filteredNotes: Note[]= new Array<Note>();
 
+  @ViewChild('filterInput') filterInputElRef: ElementRef<HTMLInputElement>;
   constructor(private notesService: NotesService ) { }
 
   ngOnInit(): void {
     // We want to retrieve all notes from the notes service.
     this.notes= this.notesService.getAll(); 
+    // this.filteredNotes=this.notesService.getAll();
+
+    this.filter('');
   }
 
-  deleteNote(id: number){
-    this.notesService.delete(id);
+  deleteNote(note: Note){
+    let noteId= this.notesService.getId(note);
+    this.notesService.delete(noteId);
+    this.filter(this.filterInputElRef.nativeElement.value);
+  }
+  
+  generateNoteURL(note: Note){
+    let noteId= this.notesService.getId(note);
+    return noteId;
+  }
+
+  filter(query: any){
+    query= query.toLowerCase().trim();
+    console.log("entered");
+    
+    let allResults: Note[] = new Array<Note>();
+    // Split up the search query into individual words.
+    let terms: string[]= query.split(' ');
+    // Remove duplicate search terms.
+    terms= this.removeDuplicates(terms);
+    // Compile all relevant results into the allResults array.
+    terms.forEach(term=>{
+      let results: Note[]= this.releventsNotes(term);
+      // Append results to the allResults array. 
+      allResults= [...allResults, ...results] // ES6
+    });
+
+    // allResults will include duplicate notes
+    // Because a prticular note can be the result of many search terms
+    // But we dont want to show the same note multiple times on the UI 
+    // So we first must remove the duplicates.
+
+    let uniqueResults= this.removeDuplicates(allResults);
+    this.filteredNotes= uniqueResults;
+
+    // Now sort by relevancy.
+    this.sortByRelevancy(allResults);
+  }
+
+  removeDuplicates(arr: Array<any>) : Array<any>{
+    let uniqueResults: Set<any> =new Set<any>;
+    arr.forEach(e=> uniqueResults.add(e));
+
+    return Array.from(uniqueResults);
+  }
+
+  releventsNotes(query: any): Array<Note>{
+    query= query.toLowerCase().trim();
+    let releventNotes= this.notes.filter(note=>{
+      if(note.title && note.title.toLowerCase().includes(query))
+        return true;
+      if(note.body && note.body.toLowerCase().includes(query))
+      return true;
+
+      return false;
+    })
+    return releventNotes;
+  }
+
+  sortByRelevancy(searchResults: Note[]){
+    // This method will calculate the relevancy of a note based on the number of times it appears 
+    // In the search results.
+
+    // Format= key: value. 
+    let noteCountObj: any= {};
+
+    searchResults.forEach(note=> {
+      let noteId= this.notesService.getId(note);
+
+      if(noteCountObj[noteId]){
+        noteCountObj[noteId]+=1;
+      }else{
+        noteCountObj[noteId]=1;
+      }
+    })
+
+    this.filteredNotes= this.filteredNotes.sort( (a: Note, b: Note) => {
+      let aId= this.notesService.getId(a);
+      let bId= this.notesService.getId(b);
+
+      let aCount= noteCountObj[aId];
+      let bCount= noteCountObj[bId];
+
+      return bCount-aCount;
+  })
+
+
   }
 }
