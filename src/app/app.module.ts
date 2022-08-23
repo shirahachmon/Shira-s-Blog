db2c04c7360bf82d41796694c5790e03cccaf4b2
shirahachmon <75565282+shirahachmon@users.noreply.github.com>
2022-08-23 19:16:04 +0300

    adding some animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { FormsModule } from '@angular/forms';
 import { LightboxModule } from 'ngx-lightbox';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+// Importing all components
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { NotesListComponent } from './notes/notes-list/notes-list.component';
+import { MainLayoutComponent } from './notes/main-layout/main-layout.component';
+import { NotesCardComponent } from './notes/notes-card/notes-card.component';
+import { NoteDetailsComponent } from './notes/note-details/note-details.component';
 import { PracticeComponent } from './practice/practice.component';
 import { TricksComponent } from './tricks/tricks.component';
 import { SummaryComponent } from './summary/summary.component';
 import { TripComponent } from './trip/trip.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+// Material
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
-import { NotesListComponent } from './notes/notes-list/notes-list.component';
-import { MainLayoutComponent } from './notes/main-layout/main-layout.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import { MatInputModule } from '@angular/material/input';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { NotesCardComponent } from './notes/notes-card/notes-card.component';
-import { NoteDetailsComponent } from './notes/note-details/note-details.component';
+import {MatInputModule } from '@angular/material/input';
+import {MatCheckboxModule } from '@angular/material/checkbox';
+
+
 
 
 @NgModule({
@@ -42,17 +49,20 @@ import { NoteDetailsComponent } from './notes/note-details/note-details.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LightboxModule,
+    FormsModule,
     BrowserAnimationsModule,
+
+    LightboxModule,
     FontAwesomeModule,
     NgbModule,
+
+    // Material imports
     MatCardModule,
     MatButtonModule,
     MatSidenavModule,
     MatInputModule,
-    MatCheckboxModule,
-    FormsModule
-    ],
+    MatCheckboxModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/notes/notes-list/notes-list.component.ts
+++ src/app/notes/notes-list/notes-list.component.ts
@@ -1,12 +1,49 @@
+import { style, transition, trigger, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import {faUserLock, faSearch} from '@fortawesome/free-solid-svg-icons';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
+
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
-  styleUrls: ['./notes-list.component.scss']
+  styleUrls: ['./notes-list.component.scss'],
+  animations: [
+    trigger('itemAnim', [
+      // Entry animation
+      transition('void => *', [
+        // Initial state
+        style({
+          height:0,
+          opacity:0,
+          transform: 'scale(0.85)', 
+          'margin-bottom' :0,
+
+          // We have to expend out the padding properties.
+          paddingTop:0,
+          paddingBotton:0,
+          paddingRight:0,
+          paddingLeft:0,
+        }),
+
+        // We first want to animate the spacing (which includes height and margin)
+        animate('50ms', style({
+          height: '*',
+          'margin-bottom': '*',
+          paddingTop :'*',
+          paddingBottom :'*',
+          paddingRight :'*',
+          paddingLeft :'*',
+        })),
+        animate(68)
+      ]) 
+
+    ])
+  
+  ]
 })
+
+
 export class NotesListComponent implements OnInit {
 
   // Importing icons
