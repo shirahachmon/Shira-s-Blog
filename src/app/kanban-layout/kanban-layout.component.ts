cff38880436f2eead29fa3c1cdec95fcca417104
shirahachmon <75565282+shirahachmon@users.noreply.github.com>
2022-08-25 13:26:13 +0300

    kanban layout category.- design and logic. using cdk angular.

---
+++ src/app/kanban-layout/kanban-layout.component.ts
@@ -0,0 +1,76 @@
+import { Board } from './../shared/board.model';
+import { Component} from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { Column } from '../shared/column.model';
+
+@Component({
+  selector: 'app-kanban-layout',
+  templateUrl: './kanban-layout.component.html',
+  styleUrls: ['./kanban-layout.component.scss']
+})
+export class KanbanLayoutComponent {
+
+
+board: Board= new Board('Test Board', [
+  new Column('Ideas', [
+    "Mabye go play tennis",
+    "Be a nails mayker",
+    "Create a facebook application",
+    "Go to rest",
+  ]),new Column('Research', [
+    "Mabye go play tennis",
+    "Be a nails mayker",
+    "Create a facebook application",
+    "Go to rest",
+  ]),new Column('Todo', [
+    'Get to work',
+    'Pick up groceries',
+    'Go home',
+    'Fall asleep'
+  ]),new Column('done', [
+    "Create a facebook application"
+  ])
+])
+
+
+
+  drop(event: CdkDragDrop<string[]>) {
+
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+}
+
+
+
+
+// todo = ['Get to work',
+// 'Pick up groceries',
+// 'Go home',
+// 'Fall asleep'];
+
+// done = ['Get up',
+// 'Brush teeth',
+// 'Take a shower',
+// 'Check e-mail',
+// 'Walk dog'];
+
+// research = ['Get up',
+// 'Brush teeth',
+// 'Take a shower',
+// 'Check e-mail',
+// 'Walk dog'];
+
+// ideas = ['Get up',
+// 'Brush teeth',
+// 'Take a shower',
+// 'Check e-mail',
+// 'Walk dog'];
